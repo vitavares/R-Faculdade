ce05c29b2fcea81919fd5f7cdea9d52b60c01119
Vitória Tavares <vitoriadesousat@gmail.com>
2023-04-02 14:38:21 -0300

    Anotaçãoes de aula

---
+++ Testes/R-Basico.R
@@ -0,0 +1,17 @@
+a = 1 # Atrinui ao objeto 'a' o valor 1; 'a' é um vetor de comprimento 1
+a # Mostra 'a'
+
+b = 2
+b
+
+c = 3
+c
+
+a; b; c # Separa as instruções da mesma linha
+
+a+b+c; a-b-c; a*b*c; a/b/c; # Operações aritméticas básicas
+
+a^b # Exponenciação
+
+ls() # Mostrar todos os objetos disponíveis.
+rm("c") # Remover objeto 'c'
--- Testes/Vetores.R
+++ Testes/Vetores.R
@@ -1,6 +1,7 @@
+rm(list = ls()) #Remover todos os objetos.
+
 x1 = 10 #Variável normal
 x1
 
 x2 = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) #Vetor; Função c() ("c" de concatenar).
 x2
-
